982a63d77c71c87031d9e83c245dbca0ded4fea2
Nandyala Sumanth Reddy <78730917+sumanthreddy76@users.noreply.github.com>
2023-06-28 22:59:09 -0500

    Time: 597 ms (25.36%), Space: 0B (100%) - LeetHub

---
+++ 0595-big-countries/0595-big-countries.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+#used AND function instead of OR  function 
+#used group by and having func which is not required 
+select name,population,area from World 
+ where population >=25000000 or  area >=3000000
