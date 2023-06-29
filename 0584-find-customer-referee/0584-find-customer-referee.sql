625746335ba0afe4295c27738001c32dd3460add
Nandyala Sumanth Reddy <78730917+sumanthreddy76@users.noreply.github.com>
2023-06-28 22:58:48 -0500

    Time: 1395 ms (8.54%), Space: 0B (100%) - LeetHub

---
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+#getting the op but not the correct one 
+#not able to use the correct funciton 
+#able to write referee_id =2 but not able to write referee_id !=2
+#should have observed question more carefully 
+
+
+select name from Customer
+where   referee_id is NULL or referee_id !=2
