06c4153bc34bf95fd70295c995aedf6383f58849
Nandyala Sumanth Reddy <78730917+sumanthreddy76@users.noreply.github.com>
2023-07-10 14:27:37 -0500

    Time: 717 ms (93.21%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -5,5 +5,19 @@
 
 # my approach (select from Weather where temperature on recordDate > temperature on recordDate)
 
-select x.id from weather x,weather y
-where x.temperature >y.temperature and DATEDIFF(x.recordDate,y.recordDate)=1
+/*select  w1.id
+from weather  as w1 
+inner join weather as  w2
+on w1.id = w2.id
+#where datediff(previous ,current =1)
+where w1.temperature > w2.temperature and 
+datediff(w1.date , w2.date) =1 */
+
+Select w2.Id
+From Weather w1 join Weather  w2
+on w2.Temperature > w1.temperature and 
+DATEDIFF(w2.recordDate, w1.recordDate)=1;
+
+#w2 is for second date if we use w1 in select it will give lowest temp dates
+#unable to solve it second time 
+#forgot all the soln
