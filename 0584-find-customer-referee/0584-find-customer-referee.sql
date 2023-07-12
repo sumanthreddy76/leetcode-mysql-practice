7a92994a5ce746707c7cec70e5ee836cadb7c910
Nandyala Sumanth Reddy <78730917+sumanthreddy76@users.noreply.github.com>
2023-07-11 21:14:03 -0500

    Time: 1007 ms (71.07%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,31 @@
+#attempt 2.unable to solve completely 3.used not null instead of null 
+
+select name from  Customer
+where referee_id is  null or referee_id <>2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Write your MySQL query statement below
 #getting the op but not the correct one 
 #not able to use the correct funciton 
@@ -5,5 +33,5 @@
 #should have observed question more carefully 
 
 
-select name from Customer
-where   referee_id is NULL or referee_id !=2
+/*select name from Customer
+where   referee_id is NULL or referee_id !=2 */
