6addc2c5a4a784d9570fed06cbec6d61c408b012
Nandyala Sumanth Reddy <78730917+sumanthreddy76@users.noreply.github.com>
2023-09-16 11:58:52 -0500

    Time: 2456 ms (20.21%), Space: 0B (100%) - LeetHub

--- 1154-product-sales-analysis-ii/1154-product-sales-analysis-ii.sql
+++ 1154-product-sales-analysis-ii/1154-product-sales-analysis-ii.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-
-
 select S.product_id,sum(quantity) as total_quantity
-from Sales S join Product P 
-on  S.product_id = P.product_id
+from Sales S 
+join product P
+on S.product_id = P.product_id
 group by S.product_id
