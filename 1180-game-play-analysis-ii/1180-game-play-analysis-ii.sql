3b3a95440bd3159ef522d42fc3b2c00e67f77c21
Nandyala Sumanth Reddy <78730917+sumanthreddy76@users.noreply.github.com>
2023-09-25 14:16:16 -0500

    Time: 981 ms (64.15%), Space: 0B (100%) - LeetHub

--- 1180-game-play-analysis-ii/1180-game-play-analysis-ii.sql
+++ 1180-game-play-analysis-ii/1180-game-play-analysis-ii.sql
@@ -1,41 +1,9 @@
 # Write your MySQL query statement below
-/*MY QUERY 
-select distinct(player_id),device_id ,first_value(device_id) over (partition by player_id order by event_date)
+/*select distinct(player_id)
+,first_value(device_id) over (partition by player_id order by event_date)device_id
+from 
+Activity*/
+
+select player_id,device_id
 from Activity
-group by device_id 
-having min(event_date) */
-
-/*select distinct player_id
-, first_value(device_id) over (partition by player_id order by event_date) device_id
-from activity */
-
-/*select distinct player_id
-,first_value(device_id) over (partition by player_id order by event_date) device_id
-from activity */
-
-
-
-
-
-
-
-
-
-/*select distinct player_id,first_value(device_id) over(partition by player_id order by event_date) device_id
-from activity */
-
-/*select player_id,device_id
- from activity 
- where (player_id,event_date) in (
-   select player_id,min(event_date)
-   
-   from activity
-   group by player_id
- ) */
-
- select distinct(player_id),first_value(device_id) over(partition by player_id order by event_date) device_id
- from activity
- 
-
-
-
+where (player_id,event_date) in (select player_id,min(event_date) from Activity group by player_id)
